dc3f817fe9557fa6c9d2c8f61b13415a63070542
tgdbamaw <mario.baumgartner1@swisscom.com>
2019-12-17 14:44:44 +0100

    axios base url for iAPC

--- src/main.js
+++ src/main.js
@@ -12,7 +12,8 @@ synctractor.init();
 synctractor.monitorFetch();
 synctractor.monitorTimeout((_, t) => t !== 11000);
 
-axios.defaults.baseURL ='http://localhost:3000';
+//axios.defaults.baseURL ='http://localhost:3001';
+axios.defaults.baseURL = 'https://tm4j2gitbackend-dev.scapp-corp.swisscom.com:3001';
 
 Vue.use(Vuelidate);
 Vue.use(VueRouter);
