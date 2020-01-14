5f42af2cca7bbe09ecb0a19b81c0cbe0f7f65f39
Marco <marco.linder@swisscom.com>
2020-01-14 16:51:57 +0100

    adaption for jenkins test

--- protractor.conf.js
+++ protractor.conf.js
@@ -6,12 +6,12 @@ const
 
 exports.config = {
     //config for local test
-    baseUrl: 'http://localhost:8080/login',
-    chromeDriver: require(`chromedriver/lib/chromedriver`).path,
+    //baseUrl: 'http://localhost:8080/login',
+    //chromeDriver: require(`chromedriver/lib/chromedriver`).path
 
     //config remote selenium grid
-    //baseUrl: 'https://tm4j2git-dev.scapp-corp.swisscom.com/',
-    //seleniumAddress: 'https://sgrid.scapp-corp.swisscom.com/wd/hub',
+    baseUrl: 'https://tm4j2git-dev.scapp-corp.swisscom.com/',
+    seleniumAddress: 'https://sgrid.scapp-corp.swisscom.com/wd/hub',
 
 
     //chromeDriver: 'chrome',
@@ -24,7 +24,7 @@ exports.config = {
     SELENIUM_PROMISE_MANAGER: false,
 
     //when using selenium Grid false is required,
-    directConnect: true,
+    directConnect: false,
 
     // https://github.com/angular/protractor/blob/master/docs/timeouts.md
     allScriptsTimeout: 110000,
