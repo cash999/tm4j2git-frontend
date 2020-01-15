df5d373e7e838dab482cc3f231f7b2c1e7d419db
tgdbamaw <mario.baumgartner1@swisscom.com>
2020-01-15 07:10:22 +0100

    Staticfile added, feature addAdmin added

--- features/support/screenplay/components/AddAdministratorComponent.ts
+++ features/support/screenplay/components/AddAdministratorComponent.ts
@@ -5,6 +5,5 @@ export class AddAdministrators {
   static addAdministrator = Target.the('Add a new administrator by account').located(by.id('addAdministrator'));
   static addAdministratorButton = Target.the('Add Admin User button').located(by.buttonText('Add Admin User'));
   static administratorList = Target.the('Administrator list').located(by.id('setAdministrator'));
-  //static administratorListValue = Target.the('Administrator list value').located(by.id("'setAdministrator'/value='TestAdministrator']"));
   // static administratorListValue = Target.the('Administrator list value').located(by.xpath("//select[@id='setAdministrator']/option[@value='TestAdministrator']"));
 }
