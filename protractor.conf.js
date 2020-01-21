a833c635fc470c2b97a442a98a3ce61eb412544a
tgdbamaw <mario.baumgartner1@swisscom.com>
2020-01-21 09:48:45 +0100

    manual sync run added

--- protractor.conf.js
+++ protractor.conf.js
@@ -9,7 +9,7 @@ exports.config = {
     baseUrl: 'https://tm4j2git-dev.scapp-corp.swisscom.com/',
     seleniumAddress: 'https://sgrid.scapp-corp.swisscom.com/wd/hub',
 
-    //chromeDriver: 'chrome',
+    chromeDriver: 'chrome',
 
     //config for jenkins test
     //baseUrl: 'https://tm4j2git-dev.scapp-corp.swisscom.com/',
