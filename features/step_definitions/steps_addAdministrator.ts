3575d73a0d88744f5cb298642baeed38b52f9df3
tgdbamaw <mario.baumgartner1@swisscom.com>
2020-01-21 09:43:08 +0100

    manual sync run added

--- features/step_definitions/steps_addAdministrator.ts
+++ features/step_definitions/steps_addAdministrator.ts
@@ -1,28 +1,28 @@
-// import {contain, Ensure, includes} from '@serenity-js/assertions';
-// import { WithStage,} from '@serenity-js/core';
-// import { isPresent, Navigate, isSelected} from '@serenity-js/protractor';
-// import { Given, Then, When } from 'cucumber';
-//
-// import {AddAdministratorByAccount} from "../support/screenplay/tasks/administration/AddAdministrator";
-// import {Administration} from "../support/screenplay/components/AdministrationComponent";
-//
-// const addAdministratorByAccount = new AddAdministratorByAccount;
-//
-// Given(/^(.*) as an administrator is on the TM4J2GIT admin page$/, function (this: WithStage, actorName: string) {
-//   return this.stage.theActorCalled(actorName).attemptsTo(
-//     Navigate.to('/admin')
-//   );
-// });
-//
-// When(/^(.*) add a new administrator by account$/, function (this: WithStage, actorName: string) {
-//   return this.stage.theActorCalled(actorName).attemptsTo(
-//     addAdministratorByAccount
-//   );
-// });
-//
-// Then(/^(.*) check the user name is listed in Administrators list$/, function (this: WithStage, actorName: string) {
-//   return this.stage.theActorCalled(actorName).attemptsTo(
-//     //Ensure.that(AddAdministrators.administratorList, isPresent())
-//     Ensure.that(Administration.administratorListValue, isSelected())
-//   );
-// });
+import {contain, Ensure, includes} from '@serenity-js/assertions';
+import { WithStage,} from '@serenity-js/core';
+import { isPresent, Navigate, isSelected} from '@serenity-js/protractor';
+import { Given, Then, When } from 'cucumber';
+
+import {AddAdministratorByAccount} from "../support/screenplay/tasks/administration/AddAdministrator";
+import {Administration} from "../support/screenplay/components/AdministrationComponent";
+
+const addAdministratorByAccount = new AddAdministratorByAccount;
+
+Given(/^(.*) as an administrator is on the TM4J2GIT admin page$/, function (this: WithStage, actorName: string) {
+  return this.stage.theActorCalled(actorName).attemptsTo(
+    Navigate.to('/admin')
+  );
+});
+
+When(/^(.*) add a new administrator by account$/, function (this: WithStage, actorName: string) {
+  return this.stage.theActorCalled(actorName).attemptsTo(
+    addAdministratorByAccount
+  );
+});
+
+Then(/^(.*) check the user name is listed in Administrators list$/, function (this: WithStage, actorName: string) {
+  return this.stage.theActorCalled(actorName).attemptsTo(
+    //Ensure.that(AddAdministrators.administratorList, isPresent())
+    Ensure.that(Administration.administratorListValue, isSelected())
+  );
+});
