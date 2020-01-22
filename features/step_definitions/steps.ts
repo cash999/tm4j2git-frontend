9d7f7dce032ca2e8b92df5dc18cc598403192d97
tgdbamaw <mario.baumgartner1@swisscom.com>
2020-01-22 10:07:32 +0100

    works only whit one feature

--- features/step_definitions/steps.ts
+++ features/step_definitions/steps.ts
@@ -1,67 +1,5 @@
-// import { Ensure } from '@serenity-js/assertions';
-// import { WithStage,} from '@serenity-js/core';
-// import { isPresent, Navigate } from '@serenity-js/protractor';
-// import { Given, Then, When } from 'cucumber';
-// import { EnterUserNameAndPassword } from '../support/screenplay/tasks/EnterUserNameAndPassword';
-// import { MySyncComponent } from "../support/screenplay/components/MySyncComponent";
-// import {AddSync} from "../support/screenplay/tasks/AddSync";
-// import {AddSyncComponent} from "../support/screenplay/components/AddSyncComponent";
-// //import {NavigateTo} from "../support/screenplay/tasks/NavigateTo";
-//
-// const enterUserNameAndPassword = new EnterUserNameAndPassword;
-// const addSync = new AddSync;
-// //const navigateTo  = new NavigateTo;
-//
-// Given(/^(.*) decides to use TM4J2GIT Synchronizer$/, function (this: WithStage, actorName: string) {
-//   return this.stage.theActorCalled(actorName).attemptsTo(
-//     Navigate.to('/login'),
-//   );
-// });
-//
-// When(/^(.*) enters user name and password$/, function (this: WithStage, actorName: string) {
-//   return this.stage.theActorCalled(actorName).attemptsTo(
-//     enterUserNameAndPassword
-//   )
-// });
-//
-// Then(/^(.*) should see the synchronisation task$/, function (this: WithStage, actorName: string) {
-//   return this.stage.theActorCalled(actorName).attemptsTo(
-//     Ensure.that(MySyncComponent.tableIsVisible, isPresent()),
-//     //Navigate.to('/'),
-//     Navigate.to('/logout')
-//   );
-// });
-//
-// Given(/^(.*) is logged into the application$/, function async(this: WithStage, actorName: string) {
-//   return this.stage.theActorCalled(actorName).attemptsTo(
-//     Navigate.to('/login'),
-//     enterUserNameAndPassword,
-//     Ensure.that(MySyncComponent.tableIsVisible, isPresent())
-//   )
-// });
-//
-// Given(/^(.*) is on the Add Sync page$/, function  (this: WithStage, actorName: string) {
-//   return this.stage.theActorCalled(actorName).attemptsTo(
-//     Navigate.to('/addSync'),
-//     //Ensure.that(AddSyncComponent.selectGITProject, isPresent())
-//   );
-// });
-//
-// When(/^(.*) enter a new Sync$/, function (this: WithStage, actorName: string) {
-//   return this.stage.theActorCalled(actorName).attemptsTo(
-//     addSync
-//   )
-// });
-//
-//
-//
-//
-
-
-
-//*****
 import { Ensure } from '@serenity-js/assertions';
-import { WithStage, } from '@serenity-js/core';
+import { WithStage,} from '@serenity-js/core';
 import { isPresent, Navigate } from '@serenity-js/protractor';
 import { Given, Then, When } from 'cucumber';
 import { EnterUserNameAndPassword } from '../support/screenplay/tasks/EnterUserNameAndPassword';
@@ -72,11 +10,11 @@ import {AddSyncComponent} from "../support/screenplay/components/AddSyncComponen
 
 const enterUserNameAndPassword = new EnterUserNameAndPassword;
 const addSync = new AddSync;
+//const navigateTo  = new NavigateTo;
 
 Given(/^(.*) decides to use TM4J2GIT Synchronizer$/, function (this: WithStage, actorName: string) {
   return this.stage.theActorCalled(actorName).attemptsTo(
     Navigate.to('/login'),
-
   );
 });
 
@@ -89,6 +27,7 @@ When(/^(.*) enters user name and password$/, function (this: WithStage, actorNam
 Then(/^(.*) should see the synchronisation task$/, function (this: WithStage, actorName: string) {
   return this.stage.theActorCalled(actorName).attemptsTo(
     Ensure.that(MySyncComponent.tableIsVisible, isPresent()),
+    //Navigate.to('/'),
     Navigate.to('/logout')
   );
 });
@@ -113,3 +52,4 @@ When(/^(.*) enter a new Sync$/, function (this: WithStage, actorName: string) {
     addSync
   )
 });
+
