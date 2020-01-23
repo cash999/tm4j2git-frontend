dc575d21c71ddd6909e1baaa20685068c34c617f
tgdbamaw <mario.baumgartner1@swisscom.com>
2020-01-23 06:57:48 +0100

    @sereinity-js/jasmine added in package.json

--- protractor_local.conf.js
+++ protractor_local.conf.js
@@ -29,7 +29,7 @@ exports.config = {
         runner: 'cucumber',
         crew: [
             ArtifactArchiver.storingArtifactsAt('./target/site/serenity'),
-            //Photographer.whoWill(TakePhotosOfInteractions),     // or Photographer.whoWill(TakePhotosOfFailures),
+            Photographer.whoWill(TakePhotosOfInteractions),     // or Photographer.whoWill(TakePhotosOfFailures),
             new SerenityBDDReporter(),
         ]
     },
