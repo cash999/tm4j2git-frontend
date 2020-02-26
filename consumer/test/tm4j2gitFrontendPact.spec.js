04ce400a5bc7c51dbd778f434660afb7a57f9f3e
TGDBAMAW <mario.baumgartner1@swisscom.com>
2020-02-26 09:54:25 +0100

    cdc tests for cdc manager added

--- consumer/test/tm4j2gitFrontendPact.spec.js
+++ consumer/test/tm4j2gitFrontendPact.spec.js
@@ -28,124 +28,128 @@ describe('act with tm4j2git backend', () => {
   });
 
   describe('given get data from backend', () => {
-    describe('when a call to get TM4J Projects from the tm4j2git backend is made', () => {
-      before(() => {
-        provider.addInteraction({
-          uponReceiving: 'a request for get projects list from TM4J',
-          withRequest: {
-            method: 'GET',
-            path: '/sync/getTm4jProjects',
-          },
-          willRespondWith: {
-            status: 200,
-            headers: {
-              'Content-Type': 'application/json; charset=utf-8',
-            },
-            body: {
-              avatarId: 24053,
-              name: "Agile Testing Service",
-              id: "21681",
-              key: "ATS"
-            },
-          },
-        })
-      });
-
-      it('can process the JSON payload from the TM4J provider', () => {
-        let url = '/sync/getTm4jProjects';
-        const response = fetchProviderData(url);
-        return expect(response).to.eventually.have.property('key', 'ATS')
-      });
-
-      it('should validate the interactions and create a contract', () => {
-        return provider.verify()
-      })
-    });
-
-    describe('when a call to get GIT Projects from the tm4j2git backend is made', () => {
-      before(() => {
-        provider.addInteraction({
-          uponReceiving: 'a request for get projects list from GIT',
-          withRequest: {
-            method: 'GET',
-            path: '/sync/getGitProjects',
-          },
-          willRespondWith: {
-            status: 200,
-            headers: {
-              'Content-Type': 'application/json; charset=utf-8',
-            },
-            body: {
-              size: 1,
-              limit: 1000,
-              isLastPage: true,
-              values: [
-                {
-                  key: "ATS",
-                  id: 3964,
-                  name: "Agile Testing Services",
-                  description: "Responsible : Weber Thomas INI-DEV-FDN-ENB",
-                  public: false,
-                  type: "NORMAL",
-                  links: {
-                    self: [
-                      {
-                        href: "https://git.swisscom.com/projects/ATS"
-                      }
-                    ]
-                  }
-                }
-              ],
-              start: 0
-            },
-          },
-        })
-      });
-
-      it('can process the JSON payload from the GIT provider', () => {
-        let url = '/sync/getGitProjects';
-        const response = fetchProviderData(url);
-        //return expect(response).to.eventually.have.property('size', 1)
-        return expect(response).to.eventually.have.property('values')
-
-      });
-
-      it('should validate the interactions and create a contract', () => {
-        return provider.verify()
-      })
-    });
+    // describe('when a call to get TM4J Projects from the tm4j2git backend is made', () => {
+    //   before(() => {
+    //     provider.addInteraction({
+    //       uponReceiving: 'a request for get projects list from TM4J',
+    //       withRequest: {
+    //         method: 'GET',
+    //         path: '/sync/getTm4jProjects',
+    //       },
+    //       willRespondWith: {
+    //         status: 200,
+    //         headers: {
+    //           'Content-Type': 'application/json; charset=utf-8',
+    //         },
+    //         body: {
+    //           avatarId: 24053,
+    //           name: "Agile Testing Service",
+    //           id: "21681",
+    //           key: "ATS"
+    //         },
+    //       },
+    //     })
+    //   });
+    //
+    //   it('can process the JSON payload from the TM4J provider', () => {
+    //     let url = '/sync/getTm4jProjects';
+    //     const response = fetchProviderData(url);
+    //     return expect(response).to.eventually.have.property('key', 'ATS')
+    //   });
+    //
+    //   it('should validate the interactions and create a contract', () => {
+    //     return provider.verify()
+    //   })
+    // });
+    //
+    // describe('when a call to get GIT Projects from the tm4j2git backend is made', () => {
+    //   before(() => {
+    //     provider.addInteraction({
+    //       uponReceiving: 'a request for get projects list from GIT',
+    //       withRequest: {
+    //         method: 'GET',
+    //         path: '/sync/getGitProjects',
+    //       },
+    //       willRespondWith: {
+    //         status: 200,
+    //         headers: {
+    //           'Content-Type': 'application/json; charset=utf-8',
+    //         },
+    //         body: {
+    //           size: 1,
+    //           limit: 1000,
+    //           isLastPage: true,
+    //           values: [
+    //             {
+    //               key: "ATS",
+    //               id: 3964,
+    //               name: "Agile Testing Services",
+    //               description: "Responsible : Weber Thomas INI-DEV-FDN-ENB",
+    //               public: false,
+    //               type: "NORMAL",
+    //               links: {
+    //                 self: [
+    //                   {
+    //                     href: "https://git.swisscom.com/projects/ATS"
+    //                   }
+    //                 ]
+    //               }
+    //             }
+    //           ],
+    //           start: 0
+    //         },
+    //       },
+    //     })
+    //   });
+    //
+    //   it('can process the JSON payload from the GIT provider', () => {
+    //     let url = '/sync/getGitProjects';
+    //     const response = fetchProviderData(url);
+    //     //return expect(response).to.eventually.have.property('size', 1)
+    //     return expect(response).to.eventually.have.property('values')
+    //
+    //   });
+    //
+    //   it('should validate the interactions and create a contract', () => {
+    //     return provider.verify()
+    //   })
+    // });
 
     describe('when a call to get Administrators from the tm4j2git backend is made', () => {
       before(() => {
-          provider.addInteraction({
-            uponReceiving: 'a request for administrator list from database',
-            withRequest: {
-              method: 'GET',
-              path: '/admin/getAdminUserList',
+        provider.addInteraction({
+          uponReceiving: 'a request for administrator list from database',
+          withRequest: {
+            method: 'GET',
+            path: '/admin/getAdminUserList',
+          },
+          willRespondWith: {
+            status: 200,
+            headers: {
+              'Content-Type': 'application/json; charset=utf-8',
             },
-            willRespondWith: {
-              status: 200,
-              headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-              },
-              body: {
-                _id: "5e312f77a21a6035a41a1db8",
-                adminUser: "SA-PF00-ATS"
-              },
-            },
-          })
-        });
-
-        it('can process the JSON payload from the Database provider', () => {
-          let url = '/admin/getAdminUserList';
-          const response = fetchProviderData(url);
-          return expect(response).to.eventually.have.property('_id', '5e312f77a21a6035a41a1db8')
-        });
-
-        it('should validate the interactions and create a contract', () => {
-          return provider.verify()
+            body: [
+              { _id: '5e312f77a21a6035a41a1db8', adminUser: 'SA-PF00-ATS' },
+              { _id: '5e312f77a21a6035a41a1db9', adminUser: 'tgdlima3' },
+              { _id: '5e312f77a21a6035a41a1dba', adminUser: 'tgdbamaw' },
+              { _id: '5e312f77a21a6035a41a1dbb', adminUser: 'tgdweth9' }
+            ]
+            ,
+          },
         })
       });
+
+      it('can process the JSON payload from the Database provider', () => {
+        let url = '/admin/getAdminUserList';
+        const response = fetchProviderData(url);
+        //return expect(response).to.eventually.have.property('_id', '5e312f77a21a6035a41a1db8')
+        return expect(response).to.eventually.be.an('array')
+      });
+
+      it('should validate the interactions and create a contract', () => {
+        return provider.verify()
+      })
+    });
 
     // Write pact files to file
     after(() => {
--- provider/test/providerPact.spec.js
+++ provider/test/providerPact.spec.js
@@ -4,54 +4,48 @@ const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 
-const { server, dataStore } = require('../provider.js');
 
-
-//Set the current state
-server.post('/setup', (req, res) => {
-  switch (req.body.state) {
-    case 'date count == 0':
-      dataStore.count = 0;
-      break;
-    default:
-      dataStore.count = 1000
-  }
-
-  res.end()
-});
-
-server.listen(8081, () => {
-  console.log('Provider service listening on http://localhost:8081')
-});
-
-// Verify that the provider meets all consumer expectations
 describe('Pact Verification', () => {
   it('should validate the expectations of tm4j2git-backend', () => {
-    let opts = {
+    let optsOrig = {
       provider: 'tm4j2git-backend',
-      providerBaseUrl: 'http://localhost:8081',
-      providerStatesUrl: 'http://localhost:8081/states',
-      providerStatesSetupUrl: 'http://localhost:8081/setup',
-      pactBrokerUrl: 'https://cdc-manager-test.scapp-corp.swisscom.com/',
+      //logLevel: "DEBUG",
+      providerBaseUrl: 'http://localhost:3000',
+      pactBrokerUrl: 'https://cdc-manager-test.scapp-corp.swisscom.com',
       consumerVersionTag: ['latest'],
       pactBrokerUsername: 'tm4j2git-twm9np4mfa',
       pactBrokerPassword: 'Kr!T3f9L2!tGJw7Wnz8$',
       publishVerificationResult: true,
       providerVersion: '1.0.0',
+      customProviderHeaders: ["Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGdkYmFtYXciLCJwYXNzd29yZCI6IlllZGFraTg1IiwiaXNMb2dnZWRJbiI6dHJ1ZSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNTgyNjE3NDg5LCJleHAiOjE1ODI2MzkwODl9.XneOSybOeGKTVyUH0URqr8J9Jo6vLPuiUIgRYHqmvks"]
     };
-    //
-    // let opts = {
-    //   provider: 'Our Provider',
-    //   providerBaseUrl: 'http://localhost:8081',
-    //   providerStatesUrl: 'http://localhost:8081/states',
-    //   providerStatesSetupUrl: 'http://localhost:8081/setup',
-    //   pactBrokerUrl: 'https://test.pact.dius.com.au',
-    //   consumerVersionTag: ['*prod'],
-    //   pactBrokerUsername: 'dXfltyFMgNOFZAxr8io9wJ37iUpY42M',
-    //   pactBrokerPassword: 'O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1',
-    //   publishVerificationResult: true,
-    //   providerVersion: '1.0.0',
-    // };
+
+    let optsExt = {
+      provider: 'tm4j2gitBackend',
+      logLevel: "DEBUG",
+      providerBaseUrl: 'http://localhost:3000',
+      pactBrokerUrl: 'https://test.pact.dius.com.au/',
+      consumerVersionTag: ['test'],
+      pactBrokerUsername: 'dXfltyFMgNOFZAxr8io9wJ37iUpY42M',
+      pactBrokerPassword: 'O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1',
+      publishVerificationResult: true,
+      providerVersion: '1.0.0',
+      customProviderHeaders: ["Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGdkYmFtYXciLCJwYXNzd29yZCI6IlllZGFraTg1IiwiaXNMb2dnZWRJbiI6dHJ1ZSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNTgyNjk1MjU0LCJleHAiOjE1ODI3MTY4NTR9.mRkagqqXeTRHqe10hQMk93SP2Nfdn3m0rhWr0WunEDg"]
+    };
+
+
+    //use local contract from path
+    let opts = {
+      provider: 'tm4j2git-backend',
+      providerBaseUrl: 'http://localhost:3000',
+      pactUrls: [
+        path.resolve(
+          process.cwd(),
+          './pacts/tm4j2git-frontend-tm4j2git-backend.json'
+        ),
+      ],
+      customProviderHeaders: ["Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoidGdkYmFtYXciLCJwYXNzd29yZCI6IlllZGFraTg1IiwiaXNMb2dnZWRJbiI6dHJ1ZSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNTgyNjk1MjU0LCJleHAiOjE1ODI3MTY4NTR9.mRkagqqXeTRHqe10hQMk93SP2Nfdn3m0rhWr0WunEDg"]
+    };
 
     return new Verifier().verifyProvider(opts).then(output => {
       console.log('Pact Verification Complete!');
